50331727aa5bf56323ccd1f12b7aa434c5c31df4
CasperSocio <casper.tollefsen@mobit.no>
2021-12-31 14:46:56 +0100

    CSS: Add shape variables

--- src/components/Alert/Alert.stories.tsx
+++ src/components/Alert/Alert.stories.tsx
@@ -1,5 +1,5 @@
-import { Meta, Story } from '@storybook/react'
 import React from 'react'
+import { Meta, Story } from '@storybook/react'
 import Alert, { AlertProps } from './Alert'
 
 export default {
